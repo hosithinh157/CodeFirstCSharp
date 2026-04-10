4d4ace4e7686d8f018057fcceb48266b7273d74c
Ho Si Thinh <thinhhosi2005@gmail.com>
2026-04-10 16:34:00 +0700

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HoSiThinh_MVC_Test.Models;
+using Microsoft.EntityFrameworkCore;
+namespace HoSiThinh_MVC_Test.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+namespace HoSiThinh_MVC_Test.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer("Server=DESKTOP-IBPKQDL\\SQLEXPRESS;Database=CodeFirstDB;Trusted_Connection=True;TrustServerCertificate=True");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
